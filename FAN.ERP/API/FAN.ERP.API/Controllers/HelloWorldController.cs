1f028df9a8fdb338f63f55bc60ce58cd7e5445ee
Fabiano Alberto Nalin <fnalin@msn.com>
2016-06-26 17:45:15 -0300

    Config API

---
+++ FAN.ERP/API/FAN.ERP.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace FAN.ERP.API.Controllers
+{
+
+    [RoutePrefix("api/v1/helloworld")]
+    public class HelloWorldController:ApiController
+    {
+
+        [Route()]
+        public string Get() {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ FAN.ERP/API/FAN.ERP.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(FAN.ERP.API.Startup))]
+
+namespace FAN.ERP.API
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            ConfigureWebApi(config);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private static void ConfigureWebApi(HttpConfiguration config)
+        {
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
